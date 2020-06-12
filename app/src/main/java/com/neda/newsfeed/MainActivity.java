284f492cd146ebb5c5ba82d446b2b7140552a1fa
Neda <neda989@gmail.com>
2020-06-12 12:54:32 +0200

    Enabled view binding, added navigation graph and HomeFragment

--- app/src/main/java/com/neda/newsfeed/MainActivity.java
+++ app/src/main/java/com/neda/newsfeed/MainActivity.java
@@ -3,12 +3,25 @@ package com.neda.newsfeed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.neda.newsfeed.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
